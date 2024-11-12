8a9e2ba7a6582f727f61f7c9ae40cff647148171
A1iHassan <ali012wkout@gmail.com>
2024-11-12 23:43:49 +0200

    a commit by Ali Hassan

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -27,8 +27,8 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> None:
-    """ GET /api/v1/stats
+    """ GET /api/v1/unauthorized
     Return:
-      - the number of each objects
+      aborts with 401
     """
     abort(401)
