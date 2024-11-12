473198f027e68f35ca26250d1a4373565c91e0c0
A1iHassan <ali012wkout@gmail.com>
2024-11-13 00:52:51 +0200

    a commit by Ali Hassan

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,32 +1,49 @@
 #!/usr/bin/env python3
-"""Module for class Auth"""
-
+""" Module of Authentication
+"""
 from flask import request
-from typing import List, TypeVar, Optional
-import fnmatch
+from typing import List, TypeVar
 
 
 class Auth:
-    """Auth class for handling authorization"""
+    """ Class to manage the API authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Method to check if auth is required."""
-        if path is None:
+        """ Method for validating if endpoint requires auth """
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
 
-        if excluded_paths is None or not excluded_paths:
+        l_path = len(path)
+        if l_path == 0:
             return True
 
-        for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
-                return False
+        slash_path = True if path[l_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
 
         return True
 
-    def authorization_header(self, request=None) -> Optional[str]:
-        """Returns None for authorization header"""
-        return None
+    def authorization_header(self, request=None) -> str:
+        """ Method that handles authorization header """
+        if request is None:
+            return None
 
-    def current_user(self, request=None) -> Optional[TypeVar('User')]:
-        """Returns None for the current user"""
+        return request.headers.get("Authorization", None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Validates current user """
         return None
