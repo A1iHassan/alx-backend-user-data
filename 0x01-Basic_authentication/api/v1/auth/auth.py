6af9d700ab0c5275c01d4d262d7a404cace6e415
A1iHassan <ali012wkout@gmail.com>
2024-11-13 00:30:45 +0200

    a commit by Ali Hassan

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 """Module for class Auth"""
+
 from flask import request
-from typing import List, TypeVar
+from typing import List, TypeVar, Optional
 
 
-class Auth():
-    """
-    class Auth
-    """
+class Auth:
+    """Auth class for handling authorization"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns False"""
+        """Returns False as authentication is not required"""
         return False
 
-
-    def authorization_header(self, request=None) -> str:
-        """returns None"""
+    def authorization_header(self, request=None) -> Optional[str]:
+        """Returns None for authorization header"""
         return None
 
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """returns None"""
+    def current_user(self, request=None) -> Optional[TypeVar('User')]:
+        """Returns None for the current user"""
         return None
